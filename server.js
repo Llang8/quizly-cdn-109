b2dc49357ac0597ec086f0f141d02d5960d26324
Lucas Lang <32493084+Llang8@users.noreply.github.com>
2023-03-21 15:35:21 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const { connectDB } = require('./src/db')
+const { User, Question, Quiz, Submission } = require('./src/models')
+const setupRoutes = require('./src/routes')
+
+dotenv.config()
+connectDB()
+
+const app = express()
+
+// require('./src/routes')(app)
+setupRoutes(app)
+
+app.get('/', async (req, res) => {
+    // const user = await User.findById('641a04c42c88ea594ae9c63b')
+    // const user = await User.find({ username: 'dsmithTHEGUY' })
+    // const user = await User.findOne({ username: 'dsmithTHEGUY' })
+    // console.log(user)
+    /* const newUser = new User({
+        email: 'psmith@gmail.com',
+        password: 'wooo',
+        username: 'Patricia Smith'
+    })
+    await newUser.save() */
+    res.send('Hello WOrld')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Quizly running on port ${process.env.PORT}`)
+})
